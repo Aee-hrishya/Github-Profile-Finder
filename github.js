8cc2fdd5ff111f4f69077a42ba9b9939b52ee67d
hrishikesh <hshinde166@gmail.com>
2021-12-17 02:15:49 +0530

    Few line spacing changes made

--- github.js
+++ github.js
@@ -8,7 +8,7 @@ class Github{
     async getUser(user){
 
         //this is the response where we will get the profile response of the user, we will have a different response for the  user repo
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id = ${this.client_id}&client_secret = ${this.client_secret}`); //here we added the query strings which will be necessary to get the information from the API
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`); //here we added the query strings which will be necessary to get the information from the API
 
         const profileData = await profileResponse.json();
 
