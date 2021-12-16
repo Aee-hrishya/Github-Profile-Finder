d22db692512c2a36af98d4a090196a5654479513
hrishikesh <hshinde166@gmail.com>
2021-12-17 02:15:04 +0530

    Added the showAlert,showProfile,clearProfile functions

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //Intilializing the Github class
 const github = new Github();
 
+//intializing the UI class
+const ui = new UI();
+
 //get the search bar
 const searchUser = document.getElementById("searchUser");
 
@@ -16,10 +19,15 @@ searchUser.addEventListener("keyup", (e) => {
                 //We are checking whether the name we are typing that user is existing or not and if not existing then we want to show an alert and go on further
                 if (data.profile.message === "Not Found") {
                     //show alert
+                    ui.showAlert("User doesn't exist","alert alert-danger");
                 }
                 else {
                     //show the profile
+                    ui.showProfile(data.profile);
                 }
             });
+    }
+    else{
+        ui.clearProfile();
     }
 });
